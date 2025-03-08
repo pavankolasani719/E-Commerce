c6bf0bb1daa83f3a50b3ec0471ecbe28a18c42be
pavankolasani719 <134358138+pavankolasani719@users.noreply.github.com>
2025-03-08 14:52:01 +1100

    Create variables.tf

---
+++ terraform-infrastructure/variables.tf
@@ -0,0 +1,31 @@
+# Define the Docker image name and version
+variable "docker_image_name" {
+  description = "The Docker image name to be pushed to Docker Hub"
+  type        = string
+  default     = "yourusername/flask-ecommerce"
+}
+
+variable "docker_image_version" {
+  description = "The Docker image version (tag)"
+  type        = string
+  default     = "latest"
+}
+
+# Define Kubernetes Cluster URL and other variables if needed
+variable "k8s_cluster_url" {
+  description = "The URL for the Kubernetes cluster"
+  type        = string
+  default     = "https://localhost:6443"
+}
+
+# Define Docker Hub username and password for registry access (if private)
+variable "docker_hub_username" {
+  description = "Docker Hub username"
+  type        = string
+}
+
+variable "docker_hub_password" {
+  description = "Docker Hub password"
+  type        = string
+  sensitive   = true
+}
