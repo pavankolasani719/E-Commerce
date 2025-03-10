7e7a87a609eb88c9ea92c4d6732db26f92f38774
pavankolasani719 <134358138+pavankolasani719@users.noreply.github.com>
2025-03-10 16:25:03 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,35 @@
 
+# Define the Docker image name and version
+variable "docker_image_name" {
+  description = "The Docker image name to be pushed to Docker Hub"
+  type        = string
+  default     = "yourusername/flask-ecommerce"
+}
+
+variable "docker_image_version" {
+  description = "The Docker image version (tag)"
+  type        = string
+  default     = "latest"
+}
+
+# Define Kubernetes Cluster URL and other variables if needed
+variable "k8s_cluster_url" {
+  description = "The URL for the Kubernetes cluster"
+  type        = string
+  default     = "https://localhost:6443"
+}
+
+# Define Docker Hub username and password for registry access (if private)
+variable "docker_hub_username" {
+  description = "Docker Hub username"
+  type        = string
+  default     = "pavankolasani719"  # Set default value (optional)
+}
+
+variable "docker_hub_password" {
+  description = "Docker Hub password"
+  type        = string
+  sensitive   = true
+  default     = "Kolasani.9"  # Set default value (optional, but not recommended)
+}
+
