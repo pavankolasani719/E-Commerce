fae71f9c1a340a74105a6689b68a6fb5d041e460
pavankolasani719 <134358138+pavankolasani719@users.noreply.github.com>
2025-03-10 15:49:56 +1100

    Update outputs.tf

--- terraform-infrastructure/outputs.tf
+++ terraform-infrastructure/outputs.tf
@@ -1,14 +1,7 @@
-# Output the Docker image name and tag
 output "docker_image" {
-  value = docker_image.flask_app.image_id
+  value = docker_image.flask_app.name
 }
 
-# Output the Kubernetes service URL
-output "flask_service_url" {
-  value = "http://localhost:8080"  # Modify this based on your k3d setup
-}
-
-# Output Kubernetes Deployment details
-output "flask_deployment_name" {
-  value = kubernetes_deployment.flask_deployment.metadata[0].name
+output "service_endpoint" {
+  value = kubernetes_service.flask_service.metadata[0].name
 }
