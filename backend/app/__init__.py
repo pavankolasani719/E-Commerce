b46a66a13903b77b27b42c7b290180359e724c85
pavankolasani719 <134358138+pavankolasani719@users.noreply.github.com>
2025-03-11 18:23:02 +1100

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user:password@db/ecommerce"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:password@postgres_db:5432/ecommerce'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from app import routes
+from app import routes, models
