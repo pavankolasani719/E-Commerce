8913b6c352a73583c3f381dcfad5bcb7ef430678
pavankolasani719 <134358138+pavankolasani719@users.noreply.github.com>
2025-03-11 15:54:03 +1100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:password@postgres_db:5432/ecommerce'
+db = SQLAlchemy(app)
 
-@app.route('/')
+# Ensure the app runs within the application context
+with app.app_context():
+    db.create_all()  # Ensures tables are created at startup
+
+@app.route("/")
 def home():
-    return 'Welcome to the eCommerce Backend!'
+    return "Ecommerce Backend Running!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
